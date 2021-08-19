d688e8c5647affc679ebbc40f240699c9720a66d
Attila Bruder <bruderattila@gmail.com>
2021-08-19 22:55:53 +0200

    Adatbázis táblák migrációja. Jöjjenek létre a kellő tábláink is az adatbázisban.

---
+++ BookShop.Dal/Migrations/20210819202649_Initial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+//Ati: Az adatbázis táblák létrehozásáért felel
+namespace BookShop.Dal.Migrations
+{
+    //Ati: Ez azért lett initial mert Add-Migration Initial parancsot adtunk ki, sztem initial helyett kb bmit írhattunk volna
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    Headline = table.Column<string>(nullable: true),
+                    ShortDescription = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    DiscountedPrice = table.Column<decimal>(nullable: true),
+                    PublishYear = table.Column<int>(nullable: false),
+                    NumberOfPages = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
