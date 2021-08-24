c553c7fb8deec6d4911e56ad480e0626b64433f1
Attila Bruder <bruderattila@gmail.com>
2021-08-24 20:31:50 +0200

    Egy komment. Az "adatbázis entitások generálása adatbázisból" előtt.

--- BookShop.Dal/Entities/Book.cs
+++ BookShop.Dal/Entities/Book.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
+//Ati: ezt még mi hoztuk létre
+//A többi entitást már a meglévö adatbázisból generáltuk le a
+//Scaffold-DbContext "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BookShopDb;Integrated Security=True" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Entities -force parancsal (lásd 3. labor)
 namespace BookShop.Dal.Entities {
     public class Book {
         public int Id { get; set; }
