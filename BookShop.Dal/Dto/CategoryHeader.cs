84e77e4e880144f2937dfb4b4fff977088809549
Attila Bruder <bruderattila@gmail.com>
2021-08-29 18:32:46 +0200

    CategoryHeader.cs Dto és CategoryService.cs service hozzáadása.

---
+++ BookShop.Dal/Dto/CategoryHeader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.Dal.Dto {
+    public class CategoryHeader {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+    }
+}
---
+++ BookShop.Dal/Services/CategoryService.cs
@@ -0,0 +1,27 @@
+﻿using BookShop.Dal.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookShop.Dal.Services {
+    public class CategoryService {
+
+        public BookShopDbContext DbContext { get; }
+        public CategoryService(BookShopDbContext dbContext) {
+            DbContext = dbContext;
+        }
+
+        public IEnumerable<CategoryHeader> GetCategoryTreeAsync() {
+            var allCategories = DbContext.Category
+            .OrderBy(c => c.Order)
+            .Select(c => new CategoryHeader {
+                Id = c.Id,
+                Level = c.Order.Split(".", StringSplitOptions.None).Count(),
+                Name = c.DisplayName
+            }).ToList();
+            return allCategories;
+        }
+
+    }
+}
