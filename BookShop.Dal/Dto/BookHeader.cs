75a7b271217cfec77136076bf506d244494a5fe9
Attila Bruder <bruderattila@gmail.com>
2021-08-29 17:39:16 +0200

    Dto és a hozzá tartozó service hozzáadása a DAL projekthez. (data transfer object)

---
+++ BookShop.Dal/Dto/BookHeader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.Dal.Dto {
+
+    //Ati: Célja, hogy az adatbázis sémában történt módosításokat elfedje.A service fájlok(pl BookService.cs) lekérik az adatbázisból az adatokat,
+    //majd áttranszformálja a Dto mappában lévő fájlok mintájára(pl BookHeader.cs). Ezt fogja megkapni/látni a Web-es project(tehát a BookHeader.cs-t használja).
+    //Ennek az a lényege h ha az adatbázisséma módosul attól még a Web project fusson(hiszen az csak a Dto-ban lévő fájlokra hivatkozik, amik nem módosultak).
+    //Tehát ez kb egy transzformáció a  2 réteg között(WEB – DAL között).
+    public class BookHeader {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string ShortDescription { get; set; }
+        public decimal Price { get; set; }
+        public decimal? DiscountedPrice { get; set; }
+        public int PublishYear { get; set; }
+        public int PageNumber { get; set; }
+        public List<int> AuthorIds { get; set; }
+        public List<string> AuthorNames { get; set; }
+        public int? PublisherId { get; set; }
+        public string PublisherName { get; set; }
+        public int? CategoryId { get; set; }
+        public int NumberOfComments { get; set; }
+        public int NumberOfRatings { get; set; }
+        public double? AverageRating { get; set; }
+    }
+}
---
+++ BookShop.Dal/Services/BookService.cs
@@ -0,0 +1,43 @@
+﻿using BookShop.Dal.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Ati: itt lesznek a fájlok amik leképezik a tényleges adatbázist a Dto fájlok mintájára.
+//Pl a BookService.cs a BookHeader.cs mintájára képezi le a Book.cs entitásunkat.
+//BookService-t e kell majd regisztrálni a webes projektben (Startup.cs) a servicek közé
+
+namespace BookShop.Dal.Services {
+    public class BookService {
+
+        public BookShopDbContext DbContext { get; }
+        public BookService(BookShopDbContext dbContext) {
+            DbContext = dbContext;
+        }
+
+        //TODO: Aszinkron is megy?
+        public IEnumerable<BookHeader> GetBooks() {
+            var books = DbContext.Book.Select(b => new BookHeader {             //itt hoz létre Book entitásból BookHeader típzusú "entitást".
+                AuthorNames = b.BookAuthor.Select(ba => ba.Author.DisplayName).ToList(),
+                AuthorIds = b.BookAuthor.Select(ba => ba.AuthorId).ToList(),
+                AverageRating = b.Rating.Select(r => r.Value).Average(),
+                CategoryId = b.CategoryId,
+                DiscountedPrice = b.DiscountPrice,
+                Id = b.Id,
+                NumberOfComments = b.Comment.Count(),
+                PageNumber = b.PageNumber,
+                NumberOfRatings = b.Rating.Count(),
+                Price = b.Price,
+                PublisherName = b.Publisher.DisplayName,
+                PublisherId = b.PublisherId,
+                PublishYear = b.PublishYear,
+                ShortDescription = b.ShortDescription,
+                Subtitle = b.Subtitle,
+                Title = b.Title
+            }).ToList();
+
+            return books;
+        }
+    }
+}
