aa091cb451802e1193a069b750c66682a860464b
Attila Bruder <bruderattila@gmail.com>
2021-08-31 21:04:37 +0200

    Ez sztem ide nem fog kelleni, de majd kiderül. BookShopDbContext.cs-ben vettem ki egy warning-os részt.

--- BookShop.Dal/BookShopDbContext.cs
+++ BookShop.Dal/BookShopDbContext.cs
@@ -45,10 +45,10 @@ namespace BookShop.Dal {
         public virtual DbSet<OrderItem> OrderItem { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-            if (!optionsBuilder.IsConfigured) {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BookShopDb;Integrated Security=True");
-            }
+//            if (!optionsBuilder.IsConfigured) {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BookShopDb;Integrated Security=True");
+//            }
         }
 
         //Ati: itt generálunk könyveket
