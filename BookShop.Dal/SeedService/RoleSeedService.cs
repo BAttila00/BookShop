45edfd2579bb8934ce9e1dc2ecd6427b8d4699dd
Attila Bruder <bruderattila@gmail.com>
2021-09-03 22:40:39 +0200

    Revert "Revert "Létrehozunk egy Administrators szerepkört és benne egy admin usert.""
    This reverts commit daa3b9cb7c33e5ceb7aeb67aca943100469b588a.

---
+++ BookShop.Dal/SeedService/RoleSeedService.cs
@@ -0,0 +1,27 @@
+﻿using BookShop.Dal.SeedInterfaces;
+using BookShop.Dal.Users;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+//Ati: Itt hozzuk létre a szerepköröket? (identity szerepkörök)
+
+namespace BookShop.Dal.SeedService {
+    public class RoleSeedService : IRoleSeedService {
+
+        private readonly RoleManager<IdentityRole<int>> roleManager;
+
+        //Ati: Az identity api-val tudjuk kezelni a szerepköröket.
+        public RoleSeedService(RoleManager<IdentityRole<int>> roleManager) {
+            this.roleManager = roleManager;
+        }
+
+        //Ati: Ha az admin szerepkör nem létezik akkor hozzuk létre és adjuk hozzá a roleManager-hez
+        public async Task SeedRoleAsync() {
+            if (!await roleManager.RoleExistsAsync(Roles.Administrators))
+                await roleManager.CreateAsync(new IdentityRole<int> { Name = Roles.Administrators });
+        }
+    }
+}
---
+++ BookShop.Dal/Users/Roles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.Dal.Users {
+
+    //Ati: Arra használjuk h ne kelljen mindig leírnunk egy szerepkörnél h ö adminisztrátor v sem.
+    public class Roles {
+        public const string Administrators = nameof(Administrators);
+    }
+}
