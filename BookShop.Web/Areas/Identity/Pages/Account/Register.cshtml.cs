8a22a0882317ab1a99d262033a204575ed14a03d
Attila Bruder <bruderattila@gmail.com>
2021-09-06 18:50:48 +0200

    Regisztrációs oldal elkészítése (emailküldés is van).

--- BookShop.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookShop.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -47,9 +47,17 @@ namespace BookShop.Web.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [Display(Name = "Full name")]
+            public string Name { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
+
+            [Required]
+            [Display(Name = "User name")]
+            public string UserName { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
@@ -75,7 +83,7 @@ namespace BookShop.Web.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = Input.Email, Email = Input.Email };
+                var user = new User { UserName = Input.UserName, Email = Input.Email, DisplayName = Input.Name };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
