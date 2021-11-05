69f1a86869a15e449482c5bc2d2bcae047dbbb8a
Attila Bruder <bruderattila@gmail.com>
2021-11-05 20:50:08 +0100

    Adding comments. basic stuff.

--- BookShop.Web/Pages/Book.cshtml.cs
+++ BookShop.Web/Pages/Book.cshtml.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookShop.Dal;
 using BookShop.Dal.Dto;
+using BookShop.Dal.Entities;
 using BookShop.Dal.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,20 +14,24 @@ namespace BookShop.Web.Pages
     public class BookModel : PageModel
     {
         private readonly BookService bookService;
+        private readonly BookShopDbContext _context;
 
         [BindProperty( SupportsGet = true)]
         public int Id { get; set; }
 
         public BookHeader Book { get; set; }
+        public List<Comment> Comments { get; set; }
 
-        public BookModel( BookService bookService)
+        public BookModel( BookService bookService, BookShopDbContext context)
         {
+            _context = context;
             this.bookService = bookService;
         }
 
         public void OnGet()
         {
             Book = bookService.GetBook(Id);
+            Comments = _context.Comment.ToList();
         }
     }
 }
--- BookShop.Web/Pages/Index.cshtml.cs
+++ BookShop.Web/Pages/Index.cshtml.cs
@@ -30,8 +30,10 @@ namespace BookShop.Web.Pages
 
         public PagedResult<BookHeader> Books { get; private set; }
         public void OnGet([FromServices] BookService bookService) {       //Ati: ez a FromServices azért kell elvileg h ne az url-böl próbálja kiszedni a context paramétert
-                                                                                           //Tehát ez a beregisztrált service-k közül elkéri a BookShopDbContext service-t (startup.cs-ben regisztráltuk be a ConfigureServices metodusban)
-                                                                                           //Ati: Az url-ből ki fogja szedni a pageNumber paramétert (pl ha ez az url http://localhost:6211/?pageNumber=3)
+                                                                          //Tehát ez a beregisztrált service-k közül elkéri a BookShopDbContext service-t (startup.cs-ben regisztráltuk be a ConfigureServices metodusban)
+                                                                          //Ati: Az url-ből ki fogja szedni a pageNumber paramétert (pl ha ez az url http://localhost:6211/?pageNumber=3)
+
+                                                                         //mondjuk ezt a konstruktorba lenne érdemesebb betenni, mint pl Book.cshtml.cs-ben is van.
             Books = bookService.GetBooks(Specification, BooksSpecification);
         }
     }
